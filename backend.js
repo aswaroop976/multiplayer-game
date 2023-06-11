6378bf38e76582b622d5c9a403e26b2e9fb404b1
aswaroop976 <66332739+aswaroop976@users.noreply.github.com>
2023-06-11 12:47:33 -0500

    Base game with some websocket stuff

---
+++ backend.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+
+// socket.io setup
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {pingInterval: 2000, pingTimeout: 5000});
+
+const port = 3000
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+const backendPlayers = {}
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  backendPlayers[socket.id] = {
+    x:500 * Math.random(),
+    y:100 * Math.random(),
+    color: `hsl(${360 * Math.random()}, 100%, 50%)`
+  }
+
+  io.emit("updatePlayers", backendPlayers)
+
+  socket.on("disconnect", (reason) => {
+    console.log(reason)
+    delete backendPlayers[socket.id]
+    io.emit("updatePlayers", backendPlayers)
+  })
+
+  console.log(backendPlayers)
+});
+
+server.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+console.log("server loaded")
---
+++ public/js/frontend.js
@@ -0,0 +1,48 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+const socket = io();
+
+const scoreEl = document.querySelector('#scoreEl')
+
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+const x = canvas.width / 2
+const y = canvas.height / 2
+
+const frontendPlayers = {}
+
+socket.on("updatePlayers", (backendPlayers)=> {
+  for(const id in backendPlayers) {
+    const backendPlayer = backendPlayers[id]
+
+    if(!frontendPlayers[id]){
+      frontendPlayers[id] = new Player({
+        x:backendPlayer.x, 
+        y:backendPlayer.y, 
+        radius: 10, 
+        color: backendPlayer.color
+      })
+    }
+  }
+
+  for(const id in frontendPlayers){
+    if(!backendPlayers[id]) {
+      delete frontendPlayers[id]
+    }
+  }
+})
+
+let animationId
+function animate() {
+  animationId = requestAnimationFrame(animate)
+  c.fillStyle = 'rgba(0, 0, 0, 0.1)'
+  c.fillRect(0, 0, canvas.width, canvas.height)
+  for(const id in frontendPlayers) {
+    const frontendPlayer = frontendPlayers[id]
+    frontendPlayer.draw()
+  }
+}
+
+animate()
